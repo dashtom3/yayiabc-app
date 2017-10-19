13f96049850885b3f62854369d95bcf2da5d6922
DESKTOP-CVR7DFH\25673 <809457175@qq.com>
2017-10-19 17:57:28 +0800

    提交

--- src/components/Wap/index/index.vue
+++ src/components/Wap/index/index.vue
@@ -63,7 +63,7 @@ export default {
   },
   created: function() {
     var that = this;
-    if (sessionStorage.getItem('isShow')=== '') {
+    if (sessionStorage.getItem('isShow')=== null) {
       if (window.plus) {
         that.plusReady();
       } else {
