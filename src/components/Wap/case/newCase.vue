e32acc6cded18b0756ef6d42738b105589d9afbb
ggkula <s335631892@icloud.com>
2017-11-07 11:29:34 +0800

    提交

--- src/components/Wap/case/newCase.vue
+++ src/components/Wap/case/newCase.vue
@@ -1,7 +1,13 @@
 <template>
   <div class="header">
-
+    <div class="backIco">返回</div>
+    <div class="saveAndRelease">
+      <div class="release">发布</div>
+      <div class="save">保存</div>
+      <div class="clr"></div>
+    </div>
   </div>
+
 </template>
 
 <script type="text/ecmascript-6">
@@ -23,5 +29,22 @@
     background-color: $themeColor;
     color: #fff;
     font-size: px2vw(32);
+    .backIco{
+      float: left;
+      width: px2vw(150);
+      display: inline-block;
+    }
+    .saveAndRelease{
+      .save{
+        float: right;
+        display: inline-block;
+        width: px2vw(100);
+      }
+      .release{
+        float: right;
+        display: inline-block;
+        width: px2vw(100);
+      }
+    }
   }
 </style>
