8666c37f0c99951a87e817f33877da633481b6cc
ggkula <s335631892@icloud.com>
2017-12-12 17:31:42 +0800

    提交

---
+++ src/components/Wap/index/database/database.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <div class="header">
+      <span class="back-click-area" @click="goBack"></span>
+      <div class="headerTitle">资料库</div>
+      <span class="search-click-area"></span>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+
+  export default {
+    data(){
+      return{
+
+      }
+    },
+
+  }
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+  @import "../../../../common/sass/factory";
+
+  .header {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: px2vw(88);
+    z-index: 999;
+    line-height: px2vw(88);
+    text-align: center;
+    background-color: $themeColor;
+    color: #fff;
+    font-size: px2vw(36);
+    .back-click-area {
+      position: absolute;
+      top: 0;
+      height: 100%;
+      left: 0;
+      width: px2vw(150);
+      background: url("../../../../images/logIn/back.png") px2vw(20) center no-repeat;
+      background-size: px2vw(18) px2vw(29);
+    }
+  }
+</style>
