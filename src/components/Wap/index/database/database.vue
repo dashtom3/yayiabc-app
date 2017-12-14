87f8312c3434a5218f81987bc859505c2ece408f
ggkula <s335631892@icloud.com>
2017-12-14 09:44:55 +0800

    提胶

--- src/components/Wap/index/database/database.vue
+++ src/components/Wap/index/database/database.vue
@@ -5,18 +5,66 @@
       <div class="headerTitle">资料库</div>
       <span class="search-click-area"></span>
     </div>
+    <div class="classify">
+      <div class="classifyList">
+        <span v-for="(value,index) in isSelect">{{value}}</span>
+      </div>
+      <div class="showMore" @click="changeClassifyShow">
+        +
+      </div>
+    </div>
+    <datum></datum>
+    <div v-if="classifyShow" class="classifyModule">
+      <div @click="changeClassifyShow" class="closeIt">
+        x
+      </div>
+      <div class="area">
+        <div class="title">
+          <span class="titleHead">我的分类</span>
+          <span class="titleDes">点击删除分类</span>
+        </div>
+        <div class="classifyForm">
+          <span v-for="(value,index) in isSelect" @click="addClassify">{{value}}</span>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title">
+          <span class="titleHead">推荐分类</span>
+          <span class="titleDes">点击添加分类</span>
+        </div>
+        <div class="classifyForm">
+          <span v-for="(value,index) in notSelect" @click="deleteClassify">+{{value}}</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import datum from '../../mine/collect/datum.vue'
 
   export default {
     data(){
       return{
-
+        isSelect:['1','2','3','4','5'],
+        notSelect:['6','7','8','9','10','11','12','12','14','15','16','17','18','19','20'],
+        classifyShow:false
       }
     },
-
+    components:[datum],
+    methods:{
+      addClassify(index){
+        this.isSelect.push(this.notSelect[index]);
+        this.notSelect.splice(index,1)
+      },
+      deleteClassify(index){
+        this.notSelect.push(this.isSelect[index]);
+        this.isSelect.splice(index,1)
+      },
+      changeClassifyShow(){
+        this.classifyShow = !this.classifyShow;
+      }
+    }
   }
 </script>
 
@@ -42,6 +90,15 @@
       left: 0;
       width: px2vw(150);
       background: url("../../../../images/logIn/back.png") px2vw(20) center no-repeat;
+      background-size: px2vw(18) px2vw(29);
+    }
+    .search-click-area {
+      position: absolute;
+      top: 0;
+      height: 100%;
+      right: 0;
+      width: px2vw(150);
+      background: url("../../../../images/case/caseOfIllness/search.png") px2vw(20) center no-repeat;
       background-size: px2vw(18) px2vw(29);
     }
   }
